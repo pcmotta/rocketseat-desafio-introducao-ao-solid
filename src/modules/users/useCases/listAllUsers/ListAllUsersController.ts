07cbf288fd756e2171255d51360f5f54f336e193
Paulo Cezar Motta <paulocezarmotta@gmail.com>
2022-03-15 21:25:59 -0300

    Solução do desafio

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const user_id = request.header("user_id");
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({
+        error,
+      });
+    }
   }
 }
 
